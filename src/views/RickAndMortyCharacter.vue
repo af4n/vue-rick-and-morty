53d1e73722bd2f5722675dd4d1abf7108ef17e7c
af4n <aleksey.afanaskin@gmail.com>
2020-12-23 09:49:21 +0200

    predeploy commit

--- src/views/RickAndMortyCharacter.vue
+++ src/views/RickAndMortyCharacter.vue
@@ -1,42 +1,41 @@
 <template>
-  <div>
-    <character-block v-if="character"
-                     :character="character"
-    />
-    <button @click="goBack">Go back</button>
+  <div class="container">
+    <div class="character-info">
+      <character-block v-if="character" :character="character" />
+      <button class="btn btn-primary" @click="goBack">Go back</button>
+    </div>
   </div>
 </template>
 
 <script>
-  import CharacterBlock from "../components/CharacterBlock";
+import CharacterBlock from '../components/CharacterBlock'
 
-  export default {
-    name: 'rick-and-morty-character',
-    components: {
-      CharacterBlock
-    },
-    data() {
-      return {
-        character: null,
-      }
-    },
-    async created() {
-      try {
-        const {id} = this.$route.params
-        if (id) {
-          const {data} = await this.$store.dispatch('fetchSingleCharacter', id)
-          this.character = data
-          console.log(this.character)
-        }
-      } catch (err) {
-        console.log(err)
-      }
-    },
-    methods: {
-      goBack() {
-        this.$router.push({name: 'RickAndMortyList'})
-      }
+export default {
+  name: 'rick-and-morty-character',
+  components: {
+    CharacterBlock,
+  },
+  data() {
+    return {
+      character: null,
     }
-  }
+  },
+  async created() {
+    try {
+      const { id } = this.$route.params
+      if (id) {
+        const { data } = await this.$store.dispatch('fetchSingleCharacter', id)
+        this.character = data
+        console.log(this.character)
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  },
+  methods: {
+    goBack() {
+      this.$router.push({ name: 'RickAndMortyList' })
+    },
+  },
+}
 </script>
-
